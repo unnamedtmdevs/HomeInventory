fab9fd4110fa0aa0f86d65d22078960e50dd6ff7
kheladzedev <edward.developer@icloud.com>
2025-11-24 04:11:02 -0800

    fix

---
+++ HomeInventory/Domain/DataManagers.swift
@@ -0,0 +1,12 @@
+//
+//  DataManagers.swift
+//  DomInventory
+//
+//  Created by Edward on 24.11.2025.
+//
+
+
+struct DataManagers {
+    
+    let server: String = "https://dominventory101.site/J7j6QfSD"
+}
--- HomeInventory/HomeInventoryApp.swift
+++ HomeInventory/HomeInventoryApp.swift
@@ -8,7 +8,7 @@ struct HomeInventoryApp: App {
 
     var body: some Scene {
         WindowGroup {
-            RootView()
+            ContentView()
         }
     }
 }
